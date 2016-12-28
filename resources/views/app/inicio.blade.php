da18a2c0f08bbe3d5c003a358f2ebafdf9940635
Mauricio Suárez vega <gerencia@miproteina.com.co>
2016-12-28 16:20:20 -0500

    cambios

--- resources/views/app/inicio.blade.php
+++ resources/views/app/inicio.blade.php
@@ -9,8 +9,8 @@
         <div class="bg-lineal-gradient-blue div-full">
             <section class="overflow-hidden content-boxed">
                 <!-- Section Content -->
-                <div class="portada row">
-                    <div class="col-lg-6 col-sm flex-sm-middle">
+                <div class="portada row flex-items-xs-middle">
+                    <div class="col-lg-6 col-sm ">
                         <div class="text-xs-center text-md-left">
                             <h1 class="h1 typer font-s48 font-w600 text-white text-uppercase">Laboratorio de <br>
                                 <span class="element">Desarrollo web</span><br>
@@ -22,15 +22,10 @@
 
                         </div>
                     </div>
-                    <div class="col-md flex-sm-bottom">
+                    <div class="col-md push-30-t ">
                         <img class="img-responsive"
                              src="https://cdn.mxpnl.com/cache/717dea6a4c9c1b162d0de3ac7250158d/images/static/landing/marketing/segmentation2/segmentation-hero@2x.png"
                              alt="">
-                    </div>
-                    <div class="col-sm-12 text-xs-center flex-xs-bottom">
-                        <a class="text-white" href="#section-2">
-                            <i class="si si-arrow-down fa-4x"></i>
-                        </a>
                     </div>
                 </div>
 
@@ -39,7 +34,7 @@
         </div>
         <div id="section-2" class="row content content-full div-full bg-white">
             <div class="col-sm-12 text-xs-center">
-                <p class="h2 text-uppercase font-w600">Esto es lo que hacemos bien</p>
+                <p class="h1 text-uppercase font-w700">Esto es lo que hacemos bien</p>
             </div>
             <div class="row col-sm-12 ">
                 <div class="col-sm text-xs-center">
