7f7d0ca544880aadaa374b6c6287779847f09a20
Mauricio Suárez vega <gerencia@miproteina.com.co>
2016-12-28 20:00:45 -0500

    cambios

--- resources/views/app/inicio.blade.php
+++ resources/views/app/inicio.blade.php
@@ -12,8 +12,13 @@
                 <div class="portada row flex-items-xs-middle">
                     <div class="col-lg-6 col-sm ">
                         <div class="text-xs-center text-md-left">
-                            <h1 class="h1 typer font-s48 font-w600 text-white text-uppercase">Laboratorio de <br>
-                                <span class="element">Desarrollo web</span><br>
+                            <h1 class="h1 rw-words rw-words-1 typer font-s48 font-w600 text-white text-uppercase">Laboratorio de <br>
+
+                                    <span>Desarrollo web</span>
+                                    <span>UI/UX design</span>
+                                    <span>Optimización Web</span>
+                                    <span>Pruebas de usabilidad</span>
+                             <br>
                                 enfocado a los usuarios</h1>
                             <h2 class="h4 text-white-op  push-10-t">Integramos servicios de estratégia, planeación,
                                 diseño y
@@ -32,33 +37,101 @@
                 <!-- END Section Content -->
             </section>
         </div>
-        <div id="section-2" class="row content content-full div-full bg-white">
+
+        <!-- seccion 2 -->
+        <div id="section-2" class="row flex-items-xs-middle content content-full div-full bg-white">
             <div class="col-sm-12 text-xs-center">
                 <p class="h1 text-uppercase font-w700">Esto es lo que hacemos bien</p>
             </div>
             <div class="row col-sm-12 ">
-                <div class="col-sm text-xs-center">
-                    <h3>Optimizacion web</h3>
-                    <p></p>
+                <div class="col-sm-6 col-lg text-xs-center">
+                    <a class="block block-sombra" href="#">
+                        <div class="bg-image"
+                             style="background-image: url('http://www.worcester.ac.uk/content_images/web-development-university-worcester-course-page-header.jpg');">
+                            <div class="bg-black-op">
+                                <div class="block-content block-content-full text-center">
+                                    <h3 class="h3 text-uppercase text-white push-50-t push-50">Desarrollo <br>web</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-content block-content-full">
+                            <div class="row text-center">
+                                <div class="col-xs-12">
+                                    <div class="h4 text-muted push-5-t">Creamos aplicacion web únicas para cada uno de nuestros clientes</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-                <div class="col-sm text-xs-center">
-                    <h3>Optimizacion web</h3>
-                    <p></p>
+                <div class="col-sm-6 col-lg text-xs-center">
+                    <a class="block block-sombra" href="#">
+                        <div class="bg-image"
+                             style="background-image: url('http://mavens.c2oq37b5hwuqhat9ztdtpvbwnol17islw.netdna-cdn.com/wp-content/uploads/2015/05/webdesign-dpc86654229-1200x608.jpg');">
+                            <div class="bg-black-op">
+                                <div class="block-content block-content-full text-center">
+                                    <h3 class="h3 text-uppercase text-white push-50-t push-50">UI/UX <br>Design</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-content block-content-full">
+                            <div class="row text-center">
+                                <div class="col-xs-12">
+                                    <div class="h4 text-muted push-5-t">Creamos aplicacion web únicas para cada uno de nuestros clientes</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-                <div class="col-sm text-xs-center">
-                    <h3>Optimizacion web</h3>
-                    <p></p>
+
+                <div class="col-sm-6 col-lg text-xs-center">
+                    <a class="block block-sombra" href="#">
+                        <div class="bg-image"
+                             style="background-image: url('https://image.freepik.com/foto-gratis/mujer-sosteniendo-un-cuaderno-en-blanco-y-dibujos-de-colores_1134-74.jpg');">
+                            <div class="bg-black-op">
+                                <div class="block-content block-content-full text-center">
+                                    <h3 class="h3 text-uppercase text-white push-50-t push-50">Optimización <br>web</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-content block-content-full">
+                            <div class="row text-center">
+                                <div class="col-xs-12">
+                                    <div class="h4 text-muted push-5-t">Creamos aplicacion web únicas para cada uno de nuestros clientes</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-                <div class="col-sm text-xs-center">
-                    <h3>Optimizacion web</h3>
-                    <p></p>
+                <div class="col-sm-6 col-lg text-xs-center">
+                    <a class="block block-sombra" href="#">
+                        <div class="bg-image"
+                             style="background-image: url('https://image.freepik.com/foto-gratis/disenadores-graficos-masculinos-y-femeninos-que-interactuan-sobre-la-computadora_1170-929.jpg');">
+                            <div class="bg-black-op">
+                                <div class="block-content block-content-full text-center">
+                                    <h3 class="h3 text-uppercase text-white push-50-t push-50">Pruebas de <br> Usabilidad</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="block-content block-content-full">
+                            <div class="row text-center">
+                                <div class="col-xs-12">
+                                    <div class="h4 text-muted push-5-t">Creamos aplicacion web únicas para cada uno de nuestros clientes</div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
+            <div class="row col-sm-12 flex-items-xs-center">
+                <a href="#" class="btn btn-lg btn-primary">Ver más servicios</a>
+
+            </div>
+            <!--
             <div class="col-sm-12 flex-sm-bottom text-xs-center">
                 <a class="text-primary" href="#section-2">
                     <i class="si si-arrow-down fa-4x"></i>
                 </a>
-            </div>
+            </div>-->
         </div>
     </main>
 @stop
