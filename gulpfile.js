de102e8a1f477d5dc630452519e2f8568186249c
Mauricio Suárez vega <gerencia@miproteina.com.co>
2016-12-30 13:08:03 -0500

    cambios

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ elixir(mix => {
     mix.less('main.less', 'resources/assets/css/oneui.min.css');
     mix.styles(['bootstrap-flex.min.css', 'oneui.min.css', 'app.css'], 'public/assets/css/all.min.css');
     mix.scripts(['oneui.min.js', 'typed.min.js','tether.min.js', 'bootstrap.min.js','functions.js'], 'public/assets/js/all.min.js');
-    //mix.task('critical');
+    mix.task('critical');
     //mix.task('cache-clear');
     //mix.browserSync();
     //mix.phpUnit();
